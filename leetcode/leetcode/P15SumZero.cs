4a95aa82fd41c64092533fc4fd29d86273f1db1c
Cheng Cui <cuicheng11165@gmail.com>
2016-09-30 10:25:57 +0800

    add p15

---
+++ leetcode/leetcode/P15SumZero.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace leetcode
+{
+    class P15SumZero
+    {
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            Array.Sort(nums);
+
+            IList<IList<int>> lists = new List<IList<int>>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i > 0 && nums[i] == nums[i - 1])
+                {
+                    continue;
+                }
+                var currentSum = 0 - nums[i];//
+
+                int left = i + 1;
+                int right = nums.Length - 1;
+                while (left < right)
+                {
+                    if (nums[left] + nums[right] > currentSum)
+                    {
+                        right--;
+                    }
+                    else if (nums[left] + nums[right] < currentSum)
+                    {
+                        left++;
+                    }
+                    else
+                    {
+                        lists.Add(new List<int> { nums[i], nums[left], nums[right] });
+                        right--;
+                        left++;
+
+                        while (nums[right] == nums[right + 1] && left < right)
+                        {//当前元素和前一个相同就跳过，避免重复
+                            right--;
+                        }
+                        while (nums[left] == nums[left - 1] && left < right)
+                        {//当前元素和前一个相同就跳过，避免重复
+                            left++;
+                        }
+                    }
+                }
+            }
+
+            return lists;
+        }
+    }
+}
